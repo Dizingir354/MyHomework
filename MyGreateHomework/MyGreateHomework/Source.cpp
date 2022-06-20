c94d897f6fbfe5ba79b6f21c51f7a86ae89abf48
Dizingir354 <Dizingir355@gmail.com>
2022-06-20 13:55:43 +0300

    createProject

---
+++ MyGreateHomework/MyGreateHomework/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Начало программы" << endl;
+	
+
+
+	system("pause");
+	return 0;
+}

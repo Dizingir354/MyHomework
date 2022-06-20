85ce672e546e4da4746cacc3c5b64e6e9504f57f
Dizingir354 <Dizingir355@gmail.com>
2022-06-20 14:01:24 +0300

    Homework1

--- MyGreateHomework/MyGreateHomework/Source.cpp
+++ MyGreateHomework/MyGreateHomework/Source.cpp
@@ -1,15 +1,43 @@
 #include <iostream>
 #include <string>
 
+#include <iomanip>
+#include <fstream>
+
 using namespace std;
 
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	cout << "Начало программы" << endl;
-	
+	string OutFiles = "D:\\Шаг\\Средняя академия ШАГ\\Програмирование\\All homework\\TextAndSlova\\OutText.txt";
+	string InFiles = "D:\\Шаг\\Средняя академия ШАГ\\Програмирование\\All homework\\TextAndSlova\\InText.txt";
+	string badText = "D:\\Шаг\\Средняя академия ШАГ\\Програмирование\\All homework\\TextAndSlova\\Slova.txt";
+	ofstream fileIn;
+	ifstream fileOut;
+	ifstream fileOutBad;
+	fileOut.open(OutFiles);
+	fileOutBad.open(badText);
+	fileIn.open(InFiles);
 
+	string str;
+	string badSlovo;
+	while (!fileOut.eof())
+	{
+		getline(fileOut, str, ' ');
+		getline(fileOutBad, badSlovo, ' ');
+		for (int i = 0; i < str.size(); i++)
+		{
+			if (str[i] == '.' || str[i] == ',' || str[i] == ':' || str[i] == ';' || str[i] == '\n')
+			{
+				str.erase(i);
+			}
+		}
+		if (str != badSlovo)
+		{
+			fileIn << str << ' ';
+		}
+	}
 
 	system("pause");
 	return 0;
